aacc94eb03c0f5257f307ca21c7e6911f47419e5
theod07 <abc@def.com>
2015-07-12 14:28:36 -0700

    add jokes.py to repository

---
+++ jokes.py
@@ -0,0 +1,20 @@
+#make some bad jokes to demonstrate how to use:
+#	1. escape characters
+#	2. using single quotes and double quotes for strings
+#	3. print()'s end keyword argument
+
+print('What do you get when you cross a snowman with a vampire?')
+input()
+print('Frostbite!')
+print()
+print("What do dentists call a astronaut\'s cavity?")
+input()
+print("A black hole!")
+print()
+print("Knock knock.")
+input()
+print("who's there?")
+print('Interrupting cow.')
+input()
+print("Interrupting cow wh", end='')
+print('-MOO!')
