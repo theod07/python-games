6dcfef3a94df4b043d8a1d48af25615c62e74e20
theod07 <abc@def.com>
2015-07-12 16:07:56 -0700

    add tictactoe, sonar, reversi, cipher, bagels to repository

---
+++ cipher.py
@@ -0,0 +1,72 @@
+# Caesar Cipher to demonstrate:
+#	1. cryptography and ciphers
+#	2. encrypting and decrypting
+#	3. ciphertext, plaintext, keys, and symbols
+#	4. caesar cipher
+#	5. ASCII ordinal values
+#	6. functions chr() and ord()
+#	7. string method isalpha()
+#	8. string methods isupper() and islower()
+#	9. cryptanalysis
+#	10. brute force technique
+
+MAX_KEY_SIZE = 26
+
+def getMode():
+	while True:
+		print('Do you wish to encrypt or decrypt or brute force a message?')
+		mode = input().lower()
+		if mode in 'encrypt e decrypt d brute b'.split():
+			return mode
+		else:
+			print('Enter either "encrypt" or "e" or "decrypt" or "d" or "brute" or "b".')
+			
+def getMessage():
+	print('Enter your message: ')
+	return input()
+	
+def getKey():
+	key = 0
+	while True:
+		print('Enter the key number (1-%s)' %(MAX_KEY_SIZE))
+		key = int(input())
+		if (key >= 1 and key <= MAX_KEY_SIZE):
+			return key
+
+def getTranslatedMessage(mode, message, key):
+	if mode[0] == 'd':
+		key = -key
+		translated = ''
+		
+	for symbol in message:
+		if symbol.isalpha():
+			num = ord(symbol)
+			num += key
+			
+			if symbol.isupper():
+				if num > ord('Z'):
+					num -= 26
+				elif num < ord('A'):
+					num += 26
+			elif symbol.islower():
+				if num > ord('z'):
+					num -= 26
+				elif num < ord('a'):
+					num += 26
+
+			translated += chr(num)
+		else:
+			translated += symbol
+	return translated
+
+mode = getMode()
+message = getMessage()
+if mode[0] != 'b':
+	key = getKey()
+	
+print('Your translated text is:')
+if mode[0] != 'b':
+	print(getTranslatedMessage(mode, message, key))
+else:
+	for key in range(1, MAX_KEY_SIZE + 1):
+		print(key, getTranslatedMessage('decrypt', message, key))
